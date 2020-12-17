c4613fa5a6f6be2b999cb458a8a4bba3dbdfea21
Daniele <daniele.bedeschi94@gmail.com>
2020-12-17 17:02:29 +0100

    Commit finale con i 2 bonus

--- js/script.js
+++ js/script.js
@@ -7,18 +7,24 @@ var app = new Vue (
      kindMusic:"all"
   },
     methods: {
+
   },
     mounted: function () {
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((result) => {
      this.discs = result.data.response;
+     // Nuovo array contente il genere musicale
        for (var i = 0; i < this.discs.length; i++) {
          let kind = this.discs[i].genre;
          if (!this.generes.includes(kind)) {
            this.generes.push(kind)
          }
        }
+       // Ordine per data
+         this.discs.sort(function(a,b) {
+        return new Date(a.year).getTime() - new Date(b.year).getTime()
+        });
      });
     }
   }
