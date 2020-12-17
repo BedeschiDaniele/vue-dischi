1919498ff8311a399ee632676139fcce7f375eb0
Daniele <daniele.bedeschi94@gmail.com>
2020-12-17 16:50:21 +0100

    Commit first bonus

--- js/script.js
+++ js/script.js
@@ -13,15 +13,13 @@ var app = new Vue (
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((result) => {
      this.discs = result.data.response;
-     for (var i = 0; i < this.discs.length; i++) {
-       let kind = this.discs[i].genre;
-       if (!this.generes.includes(kind)) {
-         this.generes.push(kind)
+       for (var i = 0; i < this.discs.length; i++) {
+         let kind = this.discs[i].genre;
+         if (!this.generes.includes(kind)) {
+           this.generes.push(kind)
+         }
        }
-     }
-     console.log(this.discs);
-     console.log(this.generes);
-      });
+     });
     }
   }
 );
