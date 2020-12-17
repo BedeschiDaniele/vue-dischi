3f96a267afb59b60123718f5de6e85bd0d032b6a
Daniele <daniele.bedeschi94@gmail.com>
2020-12-17 15:57:45 +0100

    Commit select prima parte

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@ var app = new Vue (
   {
     el: '.root',
     data: {
-     discs:[]
+     discs:[],
+     generes:[]
   },
     methods: {
+      kindMusic: function () {
+      }
   },
     mounted: function () {
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((result) => {
      this.discs = result.data.response;
+     for (var i = 0; i < this.discs.length; i++) {
+       let kind = this.discs[i].genre;
+       console.log(this.kind);
+       if (!this.generes.includes(kind)) {
+         this.generes.push(kind)
+       }
+     }
      console.log(this.discs);
+     console.log(this.generes);
       });
     }
   }
